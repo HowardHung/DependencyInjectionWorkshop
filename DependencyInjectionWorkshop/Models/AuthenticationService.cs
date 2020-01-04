1d9624da0a5b827904c6238e98b2c6365ce0a74b
Howard <xbgskg@yahoo.com.tw>
2020-01-04 22:00:20 +0800

    extract method Notify

--- DependencyInjectionWorkshop/Models/AuthenticationService.cs
+++ DependencyInjectionWorkshop/Models/AuthenticationService.cs
@@ -39,11 +39,16 @@ namespace DependencyInjectionWorkshop.Models
                 LogFailedCount(accountId, httpClient);
 
                 //notify
-                string message = $"account:{accountId} try to login failed";
-                var slackClient = new SlackClient("my api token");
-                slackClient.PostMessage(response1 => { }, "my channel", message, "my bot name");
+                Notify(accountId);
                 return false;
             }
+        }
+
+        private static void Notify(string accountId)
+        {
+            string message = $"account:{accountId} try to login failed";
+            var slackClient = new SlackClient("my api token");
+            slackClient.PostMessage(response1 => { }, "my channel", message, "my bot name");
         }
 
         private static void LogFailedCount(string accountId, HttpClient httpClient)
