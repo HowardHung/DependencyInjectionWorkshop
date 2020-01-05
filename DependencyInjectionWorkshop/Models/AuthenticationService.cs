4953472bf86e14e0141994623c2bc56f5754e46b
Howard <xbgskg@yahoo.com.tw>
2020-01-05 22:43:45 +0800

    add Verify method with 3 parameter (string accountId, string password, string otp) three method from file

--- DependencyInjectionWorkshop/Models/AuthenticationService.cs
+++ DependencyInjectionWorkshop/Models/AuthenticationService.cs
@@ -1,10 +1,45 @@
-﻿using System;
+﻿using SlackAPI;
+using System;
+using System.Net.Http;
+using System.Text;
 
 namespace DependencyInjectionWorkshop.Models
 {
     public class AuthenticationService
     {
+        public bool Verify(string accountId, string password, string otp)
+        {
+            throw new NotImplementedException();
+        }
+        public void Notify(string message)
+        {
+            var slackClient = new SlackClient("my api token");
+            slackClient.PostMessage(response => { }, "my channel", message, "my bot name");
+        }
+        public string GetHash(string plainText)
+        {
+            var crypt = new System.Security.Cryptography.SHA256Managed();
+            var hash = new StringBuilder();
+            var crypto = crypt.ComputeHash(Encoding.UTF8.GetBytes(plainText));
+            foreach (var theByte in crypto)
+            {
+                hash.Append(theByte.ToString("x2"));
+            }
+            return hash.ToString();
+        }
+        public string GetOtp(string accountId)
+        {
+            var httpClient = new HttpClient() { BaseAddress = new Uri("http://joey.com/") };
+            var response = httpClient.PostAsJsonAsync("api/otps", accountId).Result;
+            if (response.IsSuccessStatusCode)
+            {
+                return response.Content.ReadAsAsync<string>().Result;
+            }
+            else
+            {
+                throw new Exception($"web api error, accountId:{accountId}");
+            }
 
-
+        }
     }
 }
