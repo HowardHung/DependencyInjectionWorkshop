e9cbd13b2b95217de2a6874cd45cc9260c1ff7b4
Howard <xbgskg@yahoo.com.tw>
2020-01-05 09:58:48 +0800

    move file

--- DependencyInjectionWorkshop/Models/Authentication.cs
+++ DependencyInjectionWorkshop/Models/Authentication.cs
@@ -2,39 +2,6 @@
 
 namespace DependencyInjectionWorkshop.Models
 {
-    public class FailedCounterDecorator : IAuthenticationService
-    {
-        private readonly IAuthenticationService _authentication;
-        private readonly IFailedCounter _failedCounter;
-
-        public FailedCounterDecorator(IAuthenticationService authentication, IFailedCounter failedCounter)
-        {
-            _authentication = authentication;
-            _failedCounter = failedCounter;
-        }
-
-        public bool Verify(string accountId, string password, string otp)
-        {
-            var isValid = _authentication.Verify(accountId, password, otp);
-            if (isValid)
-                Reset(accountId);
-            else
-                AddFailCount(accountId);
-
-            return isValid;
-        }
-
-        private void Reset(string accountId)
-        {
-            _failedCounter.Reset(accountId);
-        }
-
-        private void AddFailCount(string accountId)
-        {
-            _failedCounter.AddFailedCount(accountId);
-        }
-    }
-
     public class Authentication : IAuthenticationService
     {
         private readonly FailedCounterDecorator _failedCounterDecorator;
